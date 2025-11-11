7934020d0f86efe3e64cf50a4a4229093f63ab36
David-HaFe <david.hambach@web.de>
2025-11-11 12:54:08 +0100

    started with constraint implementation, also testing if git user credit is configured properly now

---
+++ src/constraints/Projected_position_orientation_base.cpp
@@ -0,0 +1,16 @@
+
+
+/**
+ * This class forms the base class for a constraint
+ */
+
+#include "constraints.hpp"
+
+// constructor for base class
+Projected_position_orientation_base::Projected_position_orientation_base(
+
+) {
+
+}
+
+
---
+++ src/constraints/constraints.hpp
@@ -0,0 +1,23 @@
+
+
+/**
+ * Header file for constraint classes.
+ */
+
+class Projected_position_orientation_base {
+    private:
+
+    public:
+        Projected_position_orientation_base(
+            subsystem1,
+            subsystem2,
+            constrained_axes_translation,
+            projection_pairs_rotation,
+            r_OJ_0,
+            A_IJ_0,
+            x_i1,
+            x_i2,
+        );
+}
+
+
